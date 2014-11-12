b6f94b951c52b346ecc41496fff356a555961047
Juline Lamusga <juline@gmail.com>
2014-11-11 19:44:47 -0800

    Initial Commit

---
+++ Sandbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sandbox
+//
+//  Created by Juline Lamusga on 11/11/14.
+//  Copyright (c) 2014 Juline Lamusga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sandbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sandbox
+//
+//  Created by Juline Lamusga on 11/11/14.
+//  Copyright (c) 2014 Juline Lamusga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
